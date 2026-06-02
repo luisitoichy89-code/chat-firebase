c3edb34fe2f033199fae72b3e47bd5eccf73c82a
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-02 01:56:11 -0400

    Update control.js

--- control/control.js
+++ control/control.js
@@ -1,1 +1,7 @@
 
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {};
+export const app = initializeApp(firebaseConfig);
+export const db = null;
+export const storage = null;
